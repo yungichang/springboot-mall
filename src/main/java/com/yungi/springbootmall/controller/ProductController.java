65b567beaefe575edbda0224532f1a59aa8fdc94
yung <a8621980@gmail.com>
2023-09-20 18:53:38 +0800

    商品功能-實作修改商品

--- src/main/java/com/yungi/springbootmall/controller/ProductController.java
+++ src/main/java/com/yungi/springbootmall/controller/ProductController.java
@@ -32,4 +32,19 @@ public class ProductController {
         Product product = productService.getProductById(productId);
         return  ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        Product product = productService.getProductById(productId);
+        if(product==null){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        productService.updateProduct(productId,productRequest);
+        Product updateProduct = productService.getProductById(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+
+
+    }
 }
